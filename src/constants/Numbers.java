19134bfcdcd244411b5e99417025c519d3335ee4
abdulaziz_pulatjonov@epam.com <PulaT@7343>
2023-06-26 23:42:38 +0500

    From medium article @apulatjonov

---
+++ src/constants/Numbers.java
@@ -0,0 +1,20 @@
+package constants;
+
+/**
+ * Created by: Abdulaziz Pulatjonov
+ * Date: 26/06/2023 23:05
+ */
+public class Numbers {
+    public static final String ZERO = "zero";
+    public static final String ONE = "one";
+    public static final String TWO = "two";
+    public static final String THREE = "three";
+    public static final String FOUR = "four";
+    public static final String FIVE = "five";
+    public static final String SIX = "six";
+    public static final String SEVEN = "seven";
+    public static final String EIGHT = "eight";
+    public static final String NINE = "nine";
+
+    public static final String SPACE = " - ";
+}
